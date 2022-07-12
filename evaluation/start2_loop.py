2700e33deea813290293edfce12558a1d9f92ca8
isshy-you <85348572+isshy-you@users.noreply.github.com>
2022-07-13 00:22:39 +0900

    * scenario defined on excel sheet

--- evaluation/start2_loop.py
+++ evaluation/start2_loop.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#pip install pandas
+#pip install pyQt5
+#pip install openpyxl
 
 import sys
 import os
 import subprocess
 from argparse import ArgumentParser
-from pygit2 import Repository
+# from pygit2 import Repository
+import pandas as pd
 
 def get_option(game_level, game_time, mode, random_seed, drop_interval, resultlogjson, train_yaml, predict_weight, user_name, ShapeListMax, BlockNumMax):
     argparser = ArgumentParser()
@@ -52,6 +56,11 @@ def get_python_cmd():
         return "python3"
     return "python"
 
+def read_option_from_excel():
+    # os.chdir('C:/Users/yuich/Source/tetris/evaluation')
+    excel_file_name = './evaluation/loop_exec_option.xlsx'
+    df = pd.read_excel(excel_file_name)
+    return(df)
 
 def start():
     ## define
@@ -70,11 +79,26 @@ def start():
     BLOCK_NUM_MAX = 1000
     TRAIN_YAML = "config/default.yaml"
     PREDICT_WEIGHT = "outputs/latest/best_weight.pt"
-
     branch_list=['master','master_seigot','ish05h3']
 
-    repo = Repository('.git')
-    for branch_name in branch_list:
+    # set current directory .git
+    # repo = Repository('.git')
+    df = read_option_from_excel()
+    # for branch_name in branch_list:
+    for num in range(len(df)):
+        # SEED_FIX = df.at[num,'SEED_FIX']
+        branch_name = df.at[num,'branch_name']
+        GAME_LEVEL = df.at[num,'GAME_LEVEL']
+        GAME_TIME = df.at[num,'GAME_TIME']
+        IS_MODE = df.at[num,'IS_MODE']
+        INPUT_RANDOM_SEED = df.at[num,'INPUT_RANDOM_SEED']
+        DROP_INTERVAL = df.at[num,'DROP_INTERVAL']
+        # RESULT_LOG_JSON = df.at[num,'RESULT_LOG_JSON']
+        USER_NAME = df.at[num,'USER_NAME']
+        SHAPE_LIST_MAX = df.at[num,'SHAPE_LIST_MAX']
+        BLOCK_NUM_MAX = df.at[num,'BLOCK_NUM_MAX']
+        TRAIN_YAML = df.at[num,'TRAIN_YAML']
+        PREDICT_WEIGHT = df.at[num,'PREDICT_WEIGHT']
         # branch = repo.lookup_branch(branch_name)
         # ref = repo.lookup_reference(branch.name)
         # repo.checkout(ref)
@@ -85,147 +109,144 @@ def start():
         if ret.returncode != 0:
             print('error: subprocess failed.', file=sys.stderr)
             sys.exit(1)
-        for GAME_LEVEL in [1,2,3]:
-            if GAME_LEVEL==1:
-                GAME_TIME = 999
-                BLOCK_NUM_MAX = 180
-                seed_max = 1
-                DROP_INTERVAL = 1        # drop interval
-            elif GAME_LEVEL==2:
-                GAME_TIME = 999
-                BLOCK_NUM_MAX = 180
-                seed_max = 10
-                DROP_INTERVAL = 1        # drop interval
-            elif GAME_LEVEL==3:
-                GAME_TIME = 30
-                BLOCK_NUM_MAX = -1
-                seed_max = 10
-                DROP_INTERVAL = 1         # drop interval
-            for ii in range(1,seed_max+1,1):
-                result_name = branch_name.replace('/','-').replace('_','-')\
-                            +"_"+f'{GAME_LEVEL:1}'\
-                            +"_"+f'{GAME_TIME:03}'\
-                            +"_"+f'{BLOCK_NUM_MAX:04}'\
-                            +"_"+f'{DROP_INTERVAL:03}'\
-                            +"_"+f'{INPUT_RANDOM_SEED:+03}'\
-                            +"_"+f'{ii:03}'
-                if GAME_LEVEL==1:
-                    INPUT_RANDOM_SEED = -1
-                else:
-                    if SEED_FIX == 1:
-                        INPUT_RANDOM_SEED = ii
-                    else:
-                        INPUT_RANDOM_SEED = -1
-                ## update value if args are given
-                args = get_option(GAME_LEVEL,
-                                GAME_TIME,
-                                IS_MODE,
-                                INPUT_RANDOM_SEED,
-                                DROP_INTERVAL,
-                                '', # RESULT_LOG_JSON,
-                      TRAIN_YAML,
-                      PREDICT_WEIGHT,
-                      USER_NAME,
-                      SHAPE_LIST_MAX,
-                      BLOCK_NUM_MAX)
-                if args.game_level >= 0:
-                    GAME_LEVEL = args.game_level
-                if args.game_time >= 0 or args.game_time == -1:
-                    GAME_TIME = args.game_time
-                if args.mode in ("keyboard", "gamepad", "sample", "train", "predict", "train_sample", "predict_sample"):
-                    IS_MODE = args.mode
-                if args.random_seed >= 0:
-                    INPUT_RANDOM_SEED = args.random_seed
-                if args.drop_interval > 0:
-                    DROP_INTERVAL = args.drop_interval
-                if len(args.resultlogjson) != 0:
-                    RESULT_LOG_JSON = args.resultlogjson
-                else:
-                    # RESULT_LOG_JSON = "result/"+Repository('.').head.shorthand\
-                    RESULT_LOG_JSON = "result/"+result_name+".json"
-                if len(args.user_name) != 0:
-                    USER_NAME = args.user_name
-                if args.ShapeListMax > 1:
-                    SHAPE_LIST_MAX = args.ShapeListMax
-                if args.BlockNumMax > 1:
-                    BLOCK_NUM_MAX = args.BlockNumMax
-                if len(args.train_yaml) != 0:
-                    TRAIN_YAML = args.train_yaml
-                if args.predict_weight != None:
-                    PREDICT_WEIGHT = args.predict_weight
+        # for GAME_LEVEL in [1,2,3]:
+        if GAME_LEVEL==1:
+            GAME_TIME = 999
+            BLOCK_NUM_MAX = 180
+            seed_max = 1
+            DROP_INTERVAL = 1        # drop interval
+        elif GAME_LEVEL==2:
+            GAME_TIME = 999
+            BLOCK_NUM_MAX = 180
+            seed_max = 1
+            DROP_INTERVAL = 1        # drop interval
+        elif GAME_LEVEL==3:
+            GAME_TIME = 30
+            BLOCK_NUM_MAX = -1
+            seed_max = 1
+            DROP_INTERVAL = 1         # drop interval
+        # for num in range(1,seed_max+1,1):
+        result_name = branch_name.replace('/','-').replace('_','-')\
+                    +"_"+f'{GAME_LEVEL:1}'\
+                    +"_"+f'{GAME_TIME:03}'\
+                    +"_"+f'{BLOCK_NUM_MAX:04}'\
+                    +"_"+f'{DROP_INTERVAL:03}'\
+                    +"_"+f'{INPUT_RANDOM_SEED:+03}'\
+                    +"_"+f'{num+1:03}'
+        if GAME_LEVEL==1:
+            INPUT_RANDOM_SEED = -1
+        else:
+            if SEED_FIX != 1:
+                INPUT_RANDOM_SEED = -1
+        ## update value if args are given
+        args = get_option(GAME_LEVEL,
+                        GAME_TIME,
+                        IS_MODE,
+                        INPUT_RANDOM_SEED,
+                        DROP_INTERVAL,
+                        '', # RESULT_LOG_JSON,
+                TRAIN_YAML,
+                PREDICT_WEIGHT,
+                USER_NAME,
+                SHAPE_LIST_MAX,
+                BLOCK_NUM_MAX)
+        if args.game_level >= 0:
+            GAME_LEVEL = args.game_level
+        if args.game_time >= 0 or args.game_time == -1:
+            GAME_TIME = args.game_time
+        if args.mode in ("keyboard", "gamepad", "sample", "train", "predict", "train_sample", "predict_sample"):
+            IS_MODE = args.mode
+        if args.random_seed >= 0:
+            INPUT_RANDOM_SEED = args.random_seed
+        if args.drop_interval > 0:
+            DROP_INTERVAL = args.drop_interval
+        if len(args.resultlogjson) != 0:
+            RESULT_LOG_JSON = args.resultlogjson
+        else:
+            # RESULT_LOG_JSON = "result/"+Repository('.').head.shorthand\
+            RESULT_LOG_JSON = "result/"+result_name+".json"
+        if len(args.user_name) != 0:
+            USER_NAME = args.user_name
+        if args.ShapeListMax > 1:
+            SHAPE_LIST_MAX = args.ShapeListMax
+        if args.BlockNumMax > 1:
+            BLOCK_NUM_MAX = args.BlockNumMax
+        if len(args.train_yaml) != 0:
+            TRAIN_YAML = args.train_yaml
+        if args.predict_weight != None:
+            PREDICT_WEIGHT = args.predict_weight
 
-                ## set field parameter for level 1
-                RANDOM_SEED = 0            # random seed for field
-                OBSTACLE_HEIGHT = 0        # obstacle height (blocks)
-                OBSTACLE_PROBABILITY = 0   # obstacle probability (percent)
+        ## set field parameter for level 1
+        RANDOM_SEED = 0            # random seed for field
+        OBSTACLE_HEIGHT = 0        # obstacle height (blocks)
+        OBSTACLE_PROBABILITY = 0   # obstacle probability (percent)
 
-                ## update field parameter level
-                if GAME_LEVEL == 0:   # level0
-                    GAME_TIME = -1
-                elif GAME_LEVEL == 1: # level1
-                    RANDOM_SEED = 0
-                elif GAME_LEVEL == 2: # level2
-                    RANDOM_SEED = -1
-                elif GAME_LEVEL == 3: # level3
-                    RANDOM_SEED = -1
-                    OBSTACLE_HEIGHT = 10
-                    OBSTACLE_PROBABILITY = 40
-                else:
-                    print('invalid level: ' + str(GAME_LEVEL), file=sys.stderr)
-                    sys.exit(1)
+        ## update field parameter level
+        if GAME_LEVEL == 0:   # level0
+            GAME_TIME = -1
+        elif GAME_LEVEL == 1: # level1
+            RANDOM_SEED = 0
+        elif GAME_LEVEL == 2: # level2
+            RANDOM_SEED = -1
+        elif GAME_LEVEL == 3: # level3
+            RANDOM_SEED = -1
+            OBSTACLE_HEIGHT = 10
+            OBSTACLE_PROBABILITY = 40
+        else:
+            print('invalid level: ' + str(GAME_LEVEL), file=sys.stderr)
+            sys.exit(1)
 
-                ## update random seed
-                if INPUT_RANDOM_SEED >= 0:
-                    RANDOM_SEED = INPUT_RANDOM_SEED
+        ## update random seed
+        if INPUT_RANDOM_SEED >= 0:
+            RANDOM_SEED = INPUT_RANDOM_SEED
 
-                ## print
-                print('branch:' + branch_name)
-                print('game_level: ' + str(GAME_LEVEL))
-                print('game_time: ' + str(GAME_TIME))
-                print('RANDOM_SEED: ' + str(RANDOM_SEED))
-                print('BLOCK_NUM_MAX: ' + str(BLOCK_NUM_MAX))
-                print('IS_MODE :' + str(IS_MODE))
-                print('OBSTACLE_HEIGHT: ' + str(OBSTACLE_HEIGHT))
-                print('OBSTACLE_PROBABILITY: ' + str(OBSTACLE_PROBABILITY))
-                print('USER_NAME: ' + str(USER_NAME))
-                print('SHAPE_LIST_MAX: ' + str(SHAPE_LIST_MAX))
-                print('RESULT_LOG_JSON: ' + str(RESULT_LOG_JSON))
-                print('TRAIN_YAML: ' + str(TRAIN_YAML))
-                print('PREDICT_WEIGHT: ' + str(PREDICT_WEIGHT))
+        ## print
+        print('branch:' + branch_name)
+        print('game_level: ' + str(GAME_LEVEL))
+        print('game_time: ' + str(GAME_TIME))
+        print('RANDOM_SEED: ' + str(RANDOM_SEED))
+        print('BLOCK_NUM_MAX: ' + str(BLOCK_NUM_MAX))
+        print('IS_MODE :' + str(IS_MODE))
+        print('OBSTACLE_HEIGHT: ' + str(OBSTACLE_HEIGHT))
+        print('OBSTACLE_PROBABILITY: ' + str(OBSTACLE_PROBABILITY))
+        print('USER_NAME: ' + str(USER_NAME))
+        print('SHAPE_LIST_MAX: ' + str(SHAPE_LIST_MAX))
+        print('RESULT_LOG_JSON: ' + str(RESULT_LOG_JSON))
+        print('TRAIN_YAML: ' + str(TRAIN_YAML))
+        print('PREDICT_WEIGHT: ' + str(PREDICT_WEIGHT))
 
-                os.makedirs('result', exist_ok=True)
-                ## start game
-                PYTHON_CMD = get_python_cmd()
-                cmd = PYTHON_CMD + ' ' + 'game_manager/game_manager.py' \
-			        + ' ' + '--game_time' + ' ' + str(GAME_TIME) \
-			        + ' ' + '--seed' + ' ' + str(RANDOM_SEED) \
-			        + ' ' + '--obstacle_height' + ' ' + str(OBSTACLE_HEIGHT) \
-			        + ' ' + '--obstacle_probability' + ' ' + str(OBSTACLE_PROBABILITY) \
-			        + ' ' + '--drop_interval' + ' ' + str(DROP_INTERVAL) \
-			        + ' ' + '--mode' + ' ' + str(IS_MODE) \
-			        + ' ' + '--user_name' + ' ' + str(USER_NAME) \
-			        + ' ' + '--resultlogjson' + ' ' + str(RESULT_LOG_JSON) \
-			        + ' ' + '--ShapeListMax' + ' ' + str(SHAPE_LIST_MAX) \
-			        + ' ' + '--ShapeListMax' + ' ' + str(SHAPE_LIST_MAX) \
-			        + ' ' + '--train_yaml' + ' ' + str(TRAIN_YAML) \
-			        + ' ' + '--predict_weight' + ' ' + str(PREDICT_WEIGHT) \
-			        + ' ' + '--BlockNumMax' + ' ' + str(BLOCK_NUM_MAX)
+        os.makedirs('result', exist_ok=True)
+        ## start game
+        PYTHON_CMD = get_python_cmd()
+        cmd = PYTHON_CMD + ' ' + 'game_manager/game_manager.py' \
+            + ' ' + '--game_time' + ' ' + str(GAME_TIME) \
+            + ' ' + '--seed' + ' ' + str(RANDOM_SEED) \
+            + ' ' + '--obstacle_height' + ' ' + str(OBSTACLE_HEIGHT) \
+            + ' ' + '--obstacle_probability' + ' ' + str(OBSTACLE_PROBABILITY) \
+            + ' ' + '--drop_interval' + ' ' + str(DROP_INTERVAL) \
+            + ' ' + '--mode' + ' ' + str(IS_MODE) \
+            + ' ' + '--user_name' + ' ' + str(USER_NAME) \
+            + ' ' + '--resultlogjson' + ' ' + str(RESULT_LOG_JSON) \
+            + ' ' + '--ShapeListMax' + ' ' + str(SHAPE_LIST_MAX) \
+            + ' ' + '--train_yaml' + ' ' + str(TRAIN_YAML) \
+            + ' ' + '--predict_weight' + ' ' + str(PREDICT_WEIGHT) \
+            + ' ' + '--BlockNumMax' + ' ' + str(BLOCK_NUM_MAX)
 
-                if EXEC_LOG_ON==1:
-                    # EXEC_LOG = "result/"+Repository('.').head.shorthand\
-                    EXEC_LOG = "result/"+result_name+".log"
-                    cmd = cmd + ' ' + '>'+EXEC_LOG
+        if EXEC_LOG_ON==1:
+            # EXEC_LOG = "result/"+Repository('.').head.shorthand\
+            EXEC_LOG = "result/"+result_name+".log"
+            cmd = cmd + ' ' + '>'+EXEC_LOG
 
-                ret = subprocess.run(cmd, shell=True)
-                if ret.returncode != 0:
-                    print('error: subprocess failed.', file=sys.stderr)
-                    sys.exit(1)
-                #p = subprocess.Popen(cmd, shell=True)
-                #try:
-                #    p.wait()
-                #except KeyboardInterrupt:
-                #    print("KeyboardInterrupt, call p.terminate()")
-                #    p.terminate()
+        ret = subprocess.run(cmd, shell=True)
+        if ret.returncode != 0:
+            print('error: subprocess failed.', file=sys.stderr)
+            sys.exit(1)
+        #p = subprocess.Popen(cmd, shell=True)
+        #try:
+        #    p.wait()
+        #except KeyboardInterrupt:
+        #    print("KeyboardInterrupt, call p.terminate()")
+        #    p.terminate()
 
     cmd = 'git restore ./game_manager/block_controller.py'
     print('cmd : '+cmd)
